2319a216b6c1df9d357056366ed7bc7d9914995c
hirthickroshangk <roshan7hirthick@gmail.com>
2025-07-06 18:04:27 +0530

    make cart quantity intractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,13 +73,16 @@ forEach(button => {
         quantity: 1
       });
     }
-    console.log(cart);
-     
+
+    let cartQuantity = 0;
+    cart.forEach(item => {
+      cartQuantity += item.quantity;
+    });
     
-
-  
-
-  });
+    document.querySelector('.js-cart-quantity').
+      innerHTML = cartQuantity;
+     
+    });
 });
 
 
