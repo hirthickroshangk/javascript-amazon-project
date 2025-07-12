d427d2bdcb4eda6511f5268fa7a14f58937556a5
hirthickroshangk <roshan7hirthick@gmail.com>
2025-07-13 03:10:54 +0530

    delete the html when clicking delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem)   => {
     }
   });
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${cartItem.productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,6 +103,10 @@ forEach((link) => {
   link.addEventListener('click',() => {
     const productId = link.dataset.productId;
     deleteCartItem(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   }); 
 });  
 
