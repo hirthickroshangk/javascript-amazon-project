b9100bb01684998e5e16bed40a12cebd612659fc
hirthickroshangk <roshan7hirthick@gmail.com>
2025-07-19 12:26:38 +0530

    create a file checkoutHeader.js and render checkout header html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,8 @@
-import {renderOrderSummary} from './checkout/orderSummary.js';
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
+
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
